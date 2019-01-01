1dc2163642177a9c452bec0d6f7a08ba6e30c8a2
Charles Ai <kailun.ai27@gmail.com>
2019-01-01 18:04:06 -0500

    add new item functionality complete

--- ToDoy/ToDoListViewController.swift
+++ ToDoy/ToDoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ToDoListViewController: UITableViewController {
     
     // 选定 Table View Controller 比 普通的好，可以省很多链接与 delegate 声明
-    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Dragon"]
+    var itemArray = ["Find Mike", "Buy Eggs", "Destroy Dragon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +52,42 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)  // 点击后底色不会一直灰，而是消失
         
+    }
+    
+    //MARK: - 添加新待办事项 Add New Items to the list
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        //TODO: 初始化一个放置「输入文字」的变量
+        var textField = UITextField()
+        
+        //TODO: 设定弹出窗口 alert
+        let alert = UIAlertController(title: "Add New Todoy Item", message: "", preferredStyle: .alert)
+        
+        //TODO: 设置 弹出窗口 alert 内部的按钮动作
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // what will happen once the user clicks the Add Item button on UIAlert
+            print(textField.text)
+            // 这里是按下 Add Item 递交后显示的内容
+            self.itemArray.append(textField.text!)
+            
+            //TODO: 刷新页面，否则看不到输入数据
+            self.tableView.reloadData()
+        }
+        
+        //TODO: 在 弹出窗口中设置文字输入框
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            //TODO: 把输入框的文字 赋值给 local variable -- textField，使得能在 action 中被执行
+            textField = alertTextField
+            // 注意：在这里 print 的信息，只能在弹出时显示，无法在按钮递交后显示
+        }
+        
+        //TODO: CALL 弹出窗口，使其弹出
+        alert.addAction(action)
+        
+        //TODO: 设定动画
+        present(alert, animated: true, completion: nil)
+    
     }
 }
 
